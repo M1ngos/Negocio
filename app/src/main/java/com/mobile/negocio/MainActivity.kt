4efec14e772c5ed0e3c4e11cb5aca852fe344106
M1ngos <mingoemanuel07@gmail.com>
2024-01-07 16:02:21 +0200

    Launcher Icon

--- app/src/main/java/com/mobile/negocio/MainActivity.kt
+++ app/src/main/java/com/mobile/negocio/MainActivity.kt
@@ -11,37 +11,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mobile.negocio.ui.theme.NegocioTheme
-//test
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NegocioTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NegocioTheme {
-        Greeting("Android")
     }
 }
