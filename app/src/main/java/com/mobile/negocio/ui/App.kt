5afdf50ce18e3e795ad77dbe028e6cc97582a56b
M1ngos <mingoemanuel07@gmail.com>
2024-01-15 14:17:04 +0200

    Refactoring

--- app/src/main/java/com/mobile/negocio/ui/App.kt
+++ app/src/main/java/com/mobile/negocio/ui/App.kt
@@ -1,8 +1,6 @@
 package com.mobile.negocio.ui
 
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.mobile.negocio.ui.navigation.AppNavigation
 
 @Composable
--- app/src/main/java/com/mobile/negocio/ui/views/RegisterView.kt
+++ app/src/main/java/com/mobile/negocio/ui/views/RegisterView.kt
@@ -18,8 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.mobile.negocio.R
 import com.mobile.negocio.ui.navigation.navRegistryItems
 
 
