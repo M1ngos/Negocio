abd173b55b8a4d973fba0667993f89c0400a6938
M1ngos <mingoemanuel07@gmail.com>
2024-01-30 16:10:09 +0200

    navbar lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("com.exyte:animated-navigation-bar:1.0.0")
 
     val room_version = "2.6.1"
 
