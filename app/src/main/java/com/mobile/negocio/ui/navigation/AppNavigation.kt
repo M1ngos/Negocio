1fca9a20da2e61999239deeb22b5b0fc694d6375
M1ngos <mingoemanuel07@gmail.com>
2024-02-07 16:23:13 +0200

    Refactoring

--- app/src/main/java/com/mobile/negocio/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/mobile/negocio/ui/navigation/AppNavigation.kt
@@ -1,6 +1,7 @@
 package com.mobile.negocio.ui.navigation
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -28,11 +29,13 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -71,6 +74,7 @@ import com.mobile.negocio.ui.views.RegisterScreen
 fun AppNavGraph(
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
     val navController: NavHostController = rememberNavController()
     var selectedIndex by remember { mutableIntStateOf(0) }
     val navigationBarItems = remember { NavigationBarItems.entries.toTypedArray() }
@@ -80,24 +84,31 @@ fun AppNavGraph(
         2 -> "Devedores"
         else -> {""}
     }
-
+    var searchQuery by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
 
     Scaffold(
         modifier = Modifier.padding(all = 12.dp),
         topBar = {
-                 CenterAlignedTopAppBar(
-                     title = { Text(text = selectedLabel) },
-                     navigationIcon = {
-                         IconButton(onClick = { /*TODO*/ }) {
-                             Icon(imageVector = Icons.Filled.Search, contentDescription = "")
-                         }
-                     },
-                     actions = {
-                         IconButton(onClick = { /*TODO*/ }) {
-                             Icon(imageVector = Icons.Filled.Settings, contentDescription = "")
-                         }
-                     }
-                 )
+            CenterAlignedTopAppBar(
+                title = { Text(text = selectedLabel) },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            Toast.makeText(context, "Ainda em desenvolvimento", Toast.LENGTH_SHORT).show()
+                        }
+                    ) {
+                        Icon(imageVector = Icons.Filled.Search, contentDescription = "")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, "Ainda em desenvolvimento", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Filled.Settings, contentDescription = "")
+                    }
+                }
+            )
         },
         bottomBar = {
             AnimatedNavigationBar(
@@ -199,8 +210,7 @@ fun AppNavGraph(
             }
 
             composable(route = Views.DashView.name) {
-                DashScreen(
-                )
+                DashScreen()
             }
             composable(route = Views.DebtsView.name) {
                 DebtsScreen(
