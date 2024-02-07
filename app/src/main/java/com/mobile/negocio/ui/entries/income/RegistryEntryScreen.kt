40cb3a54b79cd089985087140ea01b7b0968ceed
M1ngos <mingoemanuel07@gmail.com>
2024-02-07 11:23:52 +0200

    Get contact info from device contact list

--- app/src/main/java/com/mobile/negocio/ui/entries/income/RegistryEntryScreen.kt
+++ app/src/main/java/com/mobile/negocio/ui/entries/income/RegistryEntryScreen.kt
@@ -1,6 +1,12 @@
 package com.mobile.negocio.ui.entries.income
 
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.provider.ContactsContract
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,16 +15,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -28,6 +43,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.maxkeppeker.sheets.core.models.base.rememberSheetState
 import com.maxkeppeler.sheets.calendar.CalendarDialog
@@ -62,7 +78,7 @@ fun RegistryEntryScreen(
             AlternativeTopBar(
                 title = stringResource(RegistryEntryDestination.titleRes),
                 canNavigateBack = canNavigateBack,
-                navigateUp = onNavigateUp
+                navigateUp = onNavigateUp,
             )
         }
     ) { it ->
@@ -100,12 +116,12 @@ fun RegistryEntryBody(
 ) {
     Column(
         modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large))
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_extra_large))
     ) {
         IncomeInputForm(
             itemDetails = registryUistate.incomeDetails,
-            onValueChange = onItemValueChange,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = onItemValueChange
         )
         Button(
             onClick = onSaveClick,
@@ -127,13 +143,13 @@ fun IncomeInputForm(
     onValueChange: (IncomeDetails) -> Unit,
     enabled: Boolean = true
 ) {
+    val calendarState = rememberSheetState()
+    val context = LocalContext.current
+
     Column(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.extra_padding_small))
     ) {
-
-        val calendarState = rememberSheetState()
-
         CalendarDialog(
             state = calendarState,
             config = CalendarConfig(
@@ -144,6 +160,54 @@ fun IncomeInputForm(
                 onValueChange(itemDetails.copy(date = date.toString() ))
             }
         )
+
+        val contactPickerLauncher = rememberLauncherForActivityResult(ActivityResultContracts.PickContact()) { uri ->
+            if (uri != null) {
+                val contactInfo = getContactInfoFromUri(context.contentResolver, uri)
+                onValueChange(itemDetails.copy(name = contactInfo.name, contact = contactInfo.phoneNumber))
+            }
+        }
+
+        var isReadContactsPermissionGranted by remember { mutableStateOf(false) }
+        val requestReadContactsPermissionLauncher =
+            rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                isReadContactsPermissionGranted = isGranted
+                if (isGranted) {
+                    contactPickerLauncher.launch(null)
+                } else {
+                    Toast.makeText(context,"Sem permissão para acessar contatos",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        LaunchedEffect(Unit) {
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    android.Manifest.permission.READ_CONTACTS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                isReadContactsPermissionGranted = true
+            } else {
+                requestReadContactsPermissionLauncher.launch(android.Manifest.permission.READ_CONTACTS)
+            }
+        }
+
+        Button(
+            onClick = {
+                if (isReadContactsPermissionGranted) {
+                    contactPickerLauncher.launch(null)
+                } else {
+                    Toast.makeText(context,"Sem permissão para acessar contatos",Toast.LENGTH_SHORT).show()
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(0.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(imageVector = Icons.Default.Person, contentDescription = "Lista de contatos")
+                Text(text = "Lista de contatos", modifier = Modifier.padding(start = 4.dp))
+            }
+        }
 
         OutlinedTextField(
             value = itemDetails.name,
@@ -220,7 +284,10 @@ fun IncomeInputForm(
         Button(
             onClick = { calendarState.show() })
         {
-            Text(text = "Alterar data do registo (Opcional)")    
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(imageVector = Icons.Default.DateRange, contentDescription = "Lista de contatos")
+                Text(text = "Alterar data do registo (Opcional)",modifier = Modifier.padding(start = 4.dp))
+            }
         }
 
         Row (
@@ -245,6 +312,48 @@ fun IncomeInputForm(
             )
         }
     }
+}
+
+data class ContactInfo(val name: String, val phoneNumber: String)
+
+fun getContactInfoFromUri(contentResolver: ContentResolver, contactUri: android.net.Uri): ContactInfo {
+    // Query the contact data based on the contact URI
+    val cursor: Cursor? = contentResolver.query(contactUri, null, null, null, null)
+
+    // Default values
+    var name = ""
+    var phoneNumber = ""
+
+    cursor?.use {
+        if (it.moveToFirst()) {
+            // Retrieve the contact name
+            val nameColumnIndex: Int = it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+            name = it.getString(nameColumnIndex)
+
+            // Retrieve the contact ID
+            val contactIdColumnIndex: Int = it.getColumnIndex(ContactsContract.Contacts._ID)
+            val contactId: Long = it.getLong(contactIdColumnIndex)
+
+            // Query the phone numbers associated with the contact ID
+            val phoneCursor: Cursor? = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                arrayOf(contactId.toString()),
+                null
+            )
+
+            phoneCursor?.use { phoneCursor ->
+                if (phoneCursor.moveToFirst()) {
+                    // Retrieve the phone number
+                    val phoneNumberColumnIndex: Int =
+                        phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                    phoneNumber = phoneCursor.getString(phoneNumberColumnIndex)
+                }
+            }
+        }
+    }
+    return ContactInfo(name = name, phoneNumber = phoneNumber)
 }
 
 @Preview(showBackground = true)
